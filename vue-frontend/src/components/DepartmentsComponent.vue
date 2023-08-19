d8ffba1dc0c53a9c193683d147b92d38026e5fe9
Daniel Siegel <business.siegel@protonmail.com>
2023-08-19 20:48:15 +0200

    Make departments editable in a popup

--- vue-frontend/src/components/DepartmentsComponent.vue
+++ vue-frontend/src/components/DepartmentsComponent.vue
@@ -15,11 +15,22 @@
           <td>{{ department.name }}</td>
           <td>
             <button @click="deleteDepartment(department.id)">Delete</button>
+            <button @click="showPopup(department)">View Details</button>
           </td>
         </tr>
       </tbody>
     </table>
     <p v-if="error" class="error-message">{{ error }}</p>
+
+    <div class="popup" v-if="selectedDepartment">
+      <div class="popup-content">
+        <h2>Department Details</h2>
+        <p><strong>Id:</strong> {{ selectedDepartment.id }}</p>
+        <input v-model="editedDepartmentName" />
+        <button @click="saveEditedDepartment">Save</button>
+        <button @click="closePopup">Close</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -31,6 +42,7 @@ export default {
   data() {
     return {
       departments: null,
+      selectedDepartment: null, // To store the selected department for popup
       error: null,
     };
   },
@@ -56,8 +68,33 @@ export default {
       } catch (error) {
         console.error('Error deleting department:', error);
       }
-    }
-  }
+    },
+    showPopup(department) {
+      this.selectedDepartment = department;
+      this.editedDepartmentName = department.name; // Initialize the editable field
+    },
+     closePopup() {
+      this.selectedDepartment = null;
+    },
+
+    async saveEditedDepartment() {
+      try {
+        await axios.put(`http://localhost:8000/departments/${this.selectedDepartment.id}`, {
+          name: this.editedDepartmentName,
+        });
+
+        // Update department name in the departments array
+        const editedIndex = this.departments.findIndex(department => department.id === this.selectedDepartment.id);
+        if (editedIndex !== -1) {
+          this.departments[editedIndex].name = this.editedDepartmentName;
+        }
+
+        this.closePopup();
+      } catch (error) {
+        console.error('Error updating department:', error);
+      }
+    },
+  },
 };
 </script>
 
@@ -68,5 +105,30 @@ h3 {
 .error-message {
   color: red;
   font-weight: bold;
+}
+
+/* Popup Styles */
+.popup {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.popup-content {
+  background-color: white;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.3);
+  text-align: center;
+}
+
+.popup button {
+  margin-top: 10px;
 }
 </style>
