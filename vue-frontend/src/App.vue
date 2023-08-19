b5119f1c2640234107568960918e37dd76b220d2
Daniel Siegel <business.siegel@protonmail.com>
2023-08-19 13:39:27 +0200

    Rudimentary "styling" and added selectable tabs

--- vue-frontend/src/App.vue
+++ vue-frontend/src/App.vue
@@ -1,41 +1,73 @@
 /* eslint-disable */
 <template>
   <div id="app">
-    <button @click="loadEmployeeComponent">Load Employee Component</button>
-    <EmployeeComponent v-if="showEmployeeComponent" />
+    <div class="nav-bar">
+      <button @click="activeTab = 'employees'">Employees</button>
+      <button @click="activeTab = 'projects'">Projects</button>
+      <button @click="activeTab = 'departments'">Departments</button>
+    </div>
+    <div class="content">
+      <EmployeeComponent v-show="activeTab === 'employees'" />
+      <ProjectsComponent v-show="activeTab === 'projects'" />
+      <DepartmentsComponent v-show="activeTab === 'departments'" />
+    </div>
   </div>
 </template>
 
 
+
 <script>
 import EmployeeComponent from "@/components/EmployeeComponent.vue";
+import ProjectsComponent from "@/components/ProjectsComponent.vue";
+import DepartmentsComponent from "@/components/DepartmentsComponent.vue";
 
 export default {
   name: "app",
   components: {
     EmployeeComponent,
+    ProjectsComponent,
+    DepartmentsComponent,
   },
   data() {
     return {
-      showEmployeeComponent: false,
+      activeTab: "employees", // Default active tab
     };
-  },
-  methods: {
-    loadEmployeeComponent() {
-      this.showEmployeeComponent = true;
-    },
   },
 };
 </script>
 
 
+
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.nav-bar {
+  position: sticky;
+  top: 0;
+  background-color: #f1f1f1;
+  padding: 10px;
+}
+
+button {
+  background-color: transparent;
+  border: none;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+
+button.active {
+  background-color: #3498db;
+  color: white;
+}
+
+.content {
+  margin-top: 20px;
 }
 </style>
